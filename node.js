d61980795596e0a0f2f610e2c0027a60607556a8
sprengel <66063434+sprengeldotnet@users.noreply.github.com>
2021-02-23 09:38:01 +0100

    Update node.js

--- node.js
+++ node.js
@@ -1,16 +1,15 @@
 const http = require('http');
 http.createServer((req, res) => {
-  var options = { 
-    hostname: '1.1.1.1',       //IP Address or Domain
-    port: 80,               //Port Number
-    path: req.url,            //For example '/index.html'
-    method: req.method,       //For example 'GET'
-    headers: req.headers,     //UserAgent, Cookies etc.
+  var options = {
+    hostname: '1.1.1.1',  //IP Address or Domain
+    port: 80,             //Port Number
+    path: req.url,        //For example '/index.html'
+    method: req.method,   //For example 'GET'
+    headers: req.headers, //User Agent, Cookies etc.
   };
-  var proxy = http.request(options, function (proxy_res) {
+  var proxy = http.request(options, proxy_res => {
     res.writeHead(proxy_res.statusCode, proxy_res.headers);
     return proxy_res.pipe(res);
   });
   return req.pipe(proxy);
 }).listen(8080, "127.0.0.1");
-console.log("server is running!");
